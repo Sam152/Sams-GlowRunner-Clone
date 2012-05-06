0d09200d5ba1f2b07fa01d34bacc10c3da3a8233
Sam <samb@dhmedia.com.au>
2012-05-06 11:17:43 +0800

    Implemented Array garbage collection.

--- js/Room.js
+++ js/Room.js
@@ -4,16 +4,19 @@
 function Room(canvas){
 
 	//The speed at which the elements in the room move
-	var roomSpeed = -3;
+	var roomSpeed = -7;
 	
 	//Where x distance the room has travelled
 	var roomDistance = context.canvas.width;
-	
+
 	//The x distance the lines cover
-	var lineDistance = 0;
+	var lineDistance = -context.canvas.width;
 	
-	//The last Y position of the lines
-	var lastLineY = 0;
+	//The initial seed for the line Y setting
+	var lastLineY = 300;
+	
+	//The variance of the platform distance from eachother
+	var platformFrequencyVariance = 100;
 	
 	//The lines drawn on the room
 	var lines = [];
@@ -27,20 +30,30 @@ function Room(canvas){
 			roomDistance += roomSpeed;
 		
 			//todo, make all the elements of the room move roomSpeed units
-			lines.forEach(function(line){ 
-				//Tick the line
-				line.tick();
-			
-				//Add our room speed to the line
-				line.moveLine(roomSpeed);
-			});
-			
-			console.log(lines.length);
+			lines.forEach(function(line, index){ 
 
+				//Tick the line
+				line.tick(roomSpeed, roomDistance);
+				
+				//If the line has completed it's task
+				if(line.isOutOfBounds()){
+					//Remove it
+					lines[index] = null;
+					
+					//Remove the first element of the array
+					//Note, this will break if the lines do not function as a
+					//conventional queue, ie first in first out. If this becomes
+					//the case use Array.clean as defined in Utils.js
+					lines = lines.splice(1);
+					
+					console.log(lines);
+				}
+
+			});
 
 			//While we don't have lines to cover the whole room
 			if(-lineDistance > roomDistance){
-				
+
 				//Create a new line on the end of the array
 				var newLineKey = lines.length;
 				
@@ -48,12 +61,17 @@ function Room(canvas){
 				lines[newLineKey] = new Line(lineDistance, lastLineY);				
 
 				//Add the lines length to the line distance
-				lineDistance += lines[newLineKey].getLength() + 10;			
-			
-				//Store the y position of the line
-				lastLineY += lines[newLineKey].getY();
-			}
+				lineDistance += 
+					
+					//Keep track of the amount of space lines have covered
+					lines[newLineKey].getLength() 
+					
+					//Add a variance for overlapping and spreading apart platform
+					+ rand(-platformFrequencyVariance, 2 * platformFrequencyVariance);			
 
+				//Store the y position of the line
+				lastLineY = lines[newLineKey].getY();
+			}
 
 			//Draw the room
 			this.draw();
